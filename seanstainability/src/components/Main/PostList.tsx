2fd6a9471a10ddd6db37faa1442a0e2d21163565
seanstainability <seanstainability@seanstainabilityui-MacBookPro.local>
2021-07-28 10:27:15 +0900

    추가: 더미 데이터로 메인 페이지 구현

---
+++ seanstainability/src/components/Main/PostList.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import PostItem from 'components/Main/PostItem';
+
+const POST_ITEM_DATA = {
+  title: 'JamStack',
+  date: '2021.07.29.',
+  categories: ['Architecture'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    'https://d33wubrfki0l68.cloudfront.net/b7d16f7f3654fb8572360301e60d76df254a323e/385ec/img/svg/architecture.svg',
+  link: 'https://github.com/seanstainability',
+};
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
+`;
+
+const PostList: FunctionComponent = function () {
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  );
+};
+
+export default PostList;
--- seanstainability/src/pages/index.tsx
+++ seanstainability/src/pages/index.tsx
@@ -1,8 +1,33 @@
 import React, { FunctionComponent } from 'react';
-import Text from 'components/Text';
+import styled from '@emotion/styled';
+import GlobalStyle from 'components/Common/GlobalStyle';
+import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
+import Introduction from 'components/Main/Introduction';
+import PostList from 'components/Main/PostList';
+
+const CATEGORY_LIST = {
+  All: 5,
+  React: 3,
+  TypeScript: 2,
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />;
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <CategoryList selectedCategory="React" categoryList={CATEGORY_LIST} />
+      <PostList />
+      <Footer />
+    </Container>
+  );
 };
 
 export default IndexPage;
