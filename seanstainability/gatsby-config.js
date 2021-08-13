b2eb42a34d3246ae3ab6b1a90701e6c8da099e44
seanstainability <seanstainability@gmail.com>
2021-08-13 11:04:59 +0900

    추가중: 브라우저 동작 원리

--- seanstainability/gatsby-config.js
+++ seanstainability/gatsby-config.js
@@ -67,6 +67,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `contents`,
+        path: `${__dirname}/contents/common`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/static`,
       },
